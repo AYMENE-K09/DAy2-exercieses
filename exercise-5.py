9824b59e9bc30bbd9d2f5b878b9dd098d9e43b86
AYMENE-K09 <khalidaymene09@gmail.com>
2025-09-17 11:32:25 +0100

    Update exercise-5.py

--- exercise-5.py
+++ exercise-5.py
@@ -1,6 +1,6 @@
 import random
 
-def guessing_number(r_nmr= random.randint(1, 5), g_nmr = int(input("enter a number between 1 and 100: "))):
+def guessing_number(r_nmr= random.randint(1, 100), g_nmr = int(input("enter a number between 1 and 100: "))):
     
     if r_nmr == g_nmr:
         print(f"Correct!")
